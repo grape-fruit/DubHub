81fa8c4dc49b2a769ae682f33c159807cda44598
chaddbennett <chaddbennett@users.noreply.github.com>
2016-07-29 22:36:48 -0700

    [build] build initial xcode simulation

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ class dubhub extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Grapefruit bitches
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
